c10ae54c16033142fbac3291fbdcc9197424cf8f
axionl <axionl@aosc.io>
2018-02-12 23:30:28 +0800

    Initialization.

---
+++ config.example.py
@@ -0,0 +1,10 @@
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DB = 'dbName'
+
+NODE_HOST = 'ip:port'
+NODE_PBULIC = False
+
+CLUSTER_NAME = 'clusterName'
+CLUSTER_HOST = 'ip:port'
+CLUSTER_PUBLIC = False
